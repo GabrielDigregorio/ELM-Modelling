5ed40075c8a7b0041849a7b5954cb4b1d2728062
Gabriel <gabriel.digregorio@student.ulg.ac.be>
2018-04-21 16:11:23 +0200

    correction.m Na Nd

--- Project3/NaNd_GenerationProfile.m
+++ Project3/NaNd_GenerationProfile.m
@@ -15,12 +15,14 @@ fileID_Na = fopen( 'Na.txt', 'wt' );
 fileID_Nd = fopen( 'Nd.txt', 'wt' );
 
 if (profile == 1)
-    fprintf(fileID_Na , '%i %i\n', -t_NiO, N_a_experiment)
-    fprintf(fileID_Na , '%i %i\n', 0, N_a_experiment)
-    fprintf(fileID_Na , '%i %i\n', t_ZnO, 0)
-    fprintf(fileID_Nd , '%i %i\n', -t_NiO, 0)
-    fprintf(fileID_Nd , '%i %i\n', 0, N_d_experiment)
-    fprintf(fileID_Nd , '%i %i\n', t_ZnO, N_d_experiment)
+    fprintf(fileID_Na , '%i %i\n', -t_NiO, N_a_experiment);
+    fprintf(fileID_Na , '%i %i\n', 0-10*eps, N_a_experiment);
+    fprintf(fileID_Na , '%i %i\n', 0+10*eps, 0);
+    fprintf(fileID_Na , '%i %i\n', t_ZnO, 0);
+    fprintf(fileID_Nd , '%i %i\n', -t_NiO, 0);
+    fprintf(fileID_Nd , '%i %i\n', 0-10*eps, 0);
+    fprintf(fileID_Nd , '%i %i\n', 0+10*eps, N_d_experiment);
+    fprintf(fileID_Nd , '%i %i\n', t_ZnO, N_d_experiment);
 elseif (profile == 2)
 elseif (profile == 3)
 elseif (profile == 4)
